6b88bf4b8db294106555e79c9667d7989138ad3f
Marquis <abahmarquis@gmail.com>
2026-07-20 20:24:40 +0100

    Remove broken www redirect middleware - DNS records dont exist for www subdomains

--- middleware/domain.global.ts
+++
@@ -1,1 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => { if (process.client) { const host = window.location.hostname; if (host === 'vendor.erranders.org') { window.location.href = 'https://www.vendor.erranders.org' + to.fullPath; return; } } })
