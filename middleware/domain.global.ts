76ee017c372ad315b25f74f1045eccecaf61ac62
Marquis <abahmarquis@gmail.com>
2026-07-20 17:00:17 +0100

    Add global middleware to redirect non-www to www to fix Firebase auth

---
+++ middleware/domain.global.ts
@@ -0,0 +1,1 @@
+export default defineNuxtRouteMiddleware((to, from) => { if (process.client) { const host = window.location.hostname; if (host === 'vendor.erranders.org') { window.location.href = 'https://www.vendor.erranders.org' + to.fullPath; return; } } })
